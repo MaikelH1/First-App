ffc890ae18f0e0546ece7872a51115c7f68a340d
MaikelH1 <126578144+MaikelH1@users.noreply.github.com>
2023-05-23 15:17:54 +0200

    Add files via upload

---
+++ src/components/DrinkButtons.jsx
@@ -0,0 +1,11 @@
+export const DrinkButtons = () => {
+  return (
+    <>
+      <h1> Would you prefer tea or coffee?</h1>
+      <div className="button-group">
+        <button className="button">Tea</button>
+        <button className="button">Coffee</button>
+      </div>
+    </>
+  );
+};
